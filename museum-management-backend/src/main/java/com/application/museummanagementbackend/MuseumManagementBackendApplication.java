38db068cd58a68bf1c92b94df16b1f6ccbf2e4ad
Ratnadeep Rajendra Kharade <92khra1mst@hft-stuttgart.de>
2020-05-25 20:38:09 +0200

    Initial commit

---
+++ museum-management-backend/src/main/java/com/application/museummanagementbackend/MuseumManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.application.museummanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseumManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseumManagementBackendApplication.class, args);
+	}
+
+}
---
+++ museum-management-backend/src/test/java/com/application/museummanagementbackend/MuseumManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.museummanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
