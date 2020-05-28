ecce418935644dcbca56f814bc4abe441de46650
Ratnadeep Rajendra Kharade <92khra1mst@hft-stuttgart.de>
2020-05-28 12:39:38 +0200

    Allowed cross origin for login

--- museum-management-backend/src/main/java/com/application/museummanagementbackend/controller/LoginController.java
+++ museum-management-backend/src/main/java/com/application/museummanagementbackend/controller/LoginController.java
@@ -3,10 +3,12 @@ package com.application.museummanagementbackend.controller;
 import com.application.museummanagementbackend.model.LoginUser;
 import com.application.museummanagementbackend.repository.LoginRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(path = "/api/login")
 public class LoginController {
