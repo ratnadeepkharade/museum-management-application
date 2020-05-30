c88f78aa2fe0947de6aae1f7301bb1db51a2fa91
Priyanka Upadhye <92uppr1mst@hft-stuttgart.de>
2020-05-30 17:28:23 +0200

    connected employee component to REST api

--- museum-management-ui/src/app/main/employees/employees.component.ts
+++ museum-management-ui/src/app/main/employees/employees.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.scss']
 })
 export class EmployeesComponent implements OnInit {
+  private REST_API_SERVER = "http://localhost:8080/api/employee/employeeList";
 
-  constructor() { }
-
+  employee : any="";
+ 
+  constructor(private http:HttpClient) { }
   ngOnInit(): void {
-  }
+    this.sendGetRequest().subscribe((data: any[])=>{
+      console.log(data);
+      this.employee = data;
+      //this.products = data;
+    })  
+    //console.log( this.http.get(this.REST_API_SERVER));
+}
 
+public sendGetRequest(){
+  return this.http.get(this.REST_API_SERVER);
+
+}
 }
