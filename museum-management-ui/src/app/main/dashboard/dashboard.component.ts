b575645ed2d660420cfa0da8178e986a5661c1f0
Ratnadeep Rajendra Kharade <92khra1mst@hft-stuttgart.de>
2020-06-17 14:59:14 +0200

    UI: Added dashboard count functionality

--- museum-management-ui/src/app/main/dashboard/dashboard.component.ts
+++ museum-management-ui/src/app/main/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { RESTService } from 'src/app/services/rest.service';
+import { LoaderService } from 'src/app/services/loader.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private restService: RESTService,
+    private loaderService: LoaderService) {
+      
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getCounts();;
+  }
+  getCounts() {
+    this.getCurrentVisitorCount();
+  }
+  getCurrentVisitorCount() {
+    //this.loaderService.show();
+    this.restService.getRequest('dashboard/count/1').subscribe((data: any[]) => {
+      console.log(data);
+      //this.loaderService.hide();
+    })
   }
 
 }
